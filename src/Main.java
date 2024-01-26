faa8c4ad235d9ebbeff62a608bf7689abae64287
MadK132 <153474301+MadK132@users.noreply.github.com>
2024-01-27 04:06:31 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,18 @@ import java.util.SimpleTimeZone;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> List = new ArrayList<>();
-        List.add(new Student("Nurislam", "Toktarov",2.3));
+        List.add(new Student("Nurislam", "Toktarov",2.5));
         List.add(new Student("Saulebay", "Nurdaulet", 3.2));
-        List.add(new Employee("Danial", "Smagulov", "Bomj", 0));
-        List.add(new Employee("Madiyar","Kashkenov","WEB-Developer", 150000));
+        List.add(new Employee("Danial", "Smagulov", "Employer", 25000));
+        List.add(new Employee("Madiyar","Kashkenov","Employer", 70000));
         Collections.sort(List);
         for (Payable payable : List){
             System.out.println(payable);
         }
         }
-    public void printData(Iterable<Person>Iterator){
-
+    public void printData(Iterable<Person> List){
+        for(Person person : List){
+            System.out.println(person.toString());
+        }
     }
 }
