49da49cfe579a4c8cf3ab0eb60b9ca1ed8a52cc2
MadK132 <153474301+MadK132@users.noreply.github.com>
2024-01-29 09:59:44 +0600

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -5,9 +5,11 @@ public class Person implements Payable, Comparable<Person>{
     private String name;
     private int id;
     private String surname;
+    //Person() - no argument constructor
     public Person(){
         id = id_gen++;
     }
+    //parameterized constructor
     public Person(String name, String surname){
         this();
         setName(name);
@@ -28,21 +30,24 @@ public class Person implements Payable, Comparable<Person>{
     public int getId() {
         return id;
     }
+    //getPosition() - use a position "student" as a default return.
     public String getPosition(){
         return "Student: ";
     }
+    //toString() - use for output at the end
     @Override
     public String toString() {
         return getPosition() + id + ". " + name + " " + surname;
     }
-
+    //getPaymentAmount() - implement here to override at another subclasses
     @Override
     public double getPaymentAmount() {
         return 0;
     }
-
+    //compareTo() - to compare two doubles in list and after sort with Collection.sort(), necessary to sort with payment
     @Override
     public int compareTo(Person o) {
         return Double.compare(this.getPaymentAmount(), o.getPaymentAmount());
     }
 }
+//getter and setters to implement information and return them.
