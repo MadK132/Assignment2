b6b459e53ec49336ac5c5aee14b98ffffbe6aee1
MadK132 <153474301+MadK132@users.noreply.github.com>
2024-01-29 09:59:14 +0600

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -4,10 +4,11 @@ public class Employee extends Person implements Payable{
     private String position;
     private double salary;
     private ArrayList<Employee> employees = new ArrayList<>();
-
+    //no argument constructor
     public Employee(){
         super();
     }
+    //parameterized constructor
     public Employee(String name, String surname, String position, double salary){
         this();
         setName(name);
@@ -31,16 +32,19 @@ public class Employee extends Person implements Payable{
     public double getSalary() {
         return salary;
     }
+    //enroll2() - not necessary, just forgot to remove this method (want to create append as in lecture video)
     public void enroll2(Employee employee){
         employees.add(employee);
     }
+    //toString - using override to differentiable position in Employee subclass and return salary.
     @Override
     public String toString() {
         return super.toString() + " " + "earns " + getPaymentAmount() + " tenge";
     }
-
+    //getPaymentAmount() - using override to return salary of employer.
     @Override
     public double getPaymentAmount() {
         return salary;
     }
 }
+//getter and setters - to implement information and return them.
