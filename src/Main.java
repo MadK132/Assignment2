e44266eca59abcb0992be31488fe1a74e8dc94b2
MadK132 <153474301+MadK132@users.noreply.github.com>
2024-01-29 09:58:58 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,19 @@ import java.util.SimpleTimeZone;
 
 public class Main {
     public static void main(String[] args) {
+        //creating list to fullfill with objects
         ArrayList<Person> List = new ArrayList<>();
         List.add(new Student("Nurislam", "Toktarov",2.5));
         List.add(new Student("Saulebay", "Nurdaulet", 3.2));
         List.add(new Employee("Danial", "Smagulov", "Employer", 25000));
         List.add(new Employee("Madiyar","Kashkenov","Employer", 70000));
+        //Collections.sort() - to sort with payments
         Collections.sort(List);
+        //calling a printdata() - to output final result with sorted payments
         printData(List);
         }
-    public void printData(Iterable<Person> List){
+    //creating separate method to call in main for outputting information
+     public static void printData(Iterable<Person> List){
         for(Person person : List){
             System.out.println(person.toString());
         }
